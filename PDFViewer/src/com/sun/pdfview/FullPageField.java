bea80f4bf79ff305b17e1e09d0d75be40b6db8cd
rcmaniac25 <devnull@localhost>
2011-06-26 23:39:08 -0400

    PDFViewer demo app now actually draws the PDF full screen. The implemented graphics system isn't done so as of this version it just draws "blocks".

--- PDFViewer/src/com/sun/pdfview/FullPageField.java
+++ PDFViewer/src/com/sun/pdfview/FullPageField.java
@@ -1,3 +1,5 @@
+//#preprocessor
+
 /*
  * File: FullPageField.java
  * Version: 1.0
@@ -21,7 +23,9 @@ package com.sun.pdfview;
 
 import com.sun.pdfview.ui.GestureField;
 
+import net.rim.device.api.math.Fixed32;
 import net.rim.device.api.math.VecMath;
+import net.rim.device.api.system.Bitmap;
 import net.rim.device.api.system.Display;
 import net.rim.device.api.ui.Color;
 import net.rim.device.api.ui.Graphics;
@@ -38,6 +42,12 @@ public class FullPageField extends GestureField
 	private int[] mat;
 	
 	private PDFViewer.PDFViewerScreen viewer;
+	private PDFPage page;
+	private int curRenderWidth, curRenderHeight;
+	private Bitmap drawnPage;
+	private boolean needRerender;
+	
+	private int sW, sH;
 	
 	public FullPageField(PDFViewer.PDFViewerScreen pdfviewer)
 	{
@@ -50,45 +60,55 @@ public class FullPageField extends GestureField
 	
 	protected void layout(int width, int height)
 	{
-		setExtent(width = Math.min(width, Display.getWidth()), height = Math.min(height, Display.getHeight()));
+		setExtent(sW = Math.min(width, Display.getWidth()), sH = Math.min(height, Display.getHeight()));
+		sW = Fixed32.toFP(sW);
+		sH = Fixed32.toFP(sH);
 	}
 	
 	protected void paint(Graphics graphics)
 	{
 		int pcolor = graphics.getColor();
-		/*
-		//Create the path
-		int[] xPts = new int[4]; //U-L, U-R, B-R, B-L
-		int[] yPts = new int[4];
+		int palpha = graphics.getGlobalAlpha();
 		
-		//Setup path
-		xPts[0] = xPts[3] = this.drawRect.x;
-		xPts[1] = xPts[2] = this.drawRect.X2();
-		
-		yPts[0] = yPts[1] = this.drawRect.y;
-		yPts[2] = yPts[3] = this.drawRect.Y2();
-		
-		//Transform points
-		VecMath.transformPoints(mat, DRAWING_MATRIX, xPts, yPts, xPts, yPts);
-		
-		//Draw rectangle
-		graphics.setColor(Color.RED);
-		graphics.drawFilledPath(xPts, yPts, null, null);
-		*/
+		XYRect extent = this.getExtent();
 		if(super.gestureProcessing) //Simple way to check if a PDF is in use
 		{
-			//TODO: Draw PDF
+			//Clear the background
+			graphics.setColor(Color.GRAY);
+			graphics.fillRect(0, 0, extent.width, extent.height);
+			
+			//Create the path
+			int[] xPts = new int[]{0, 0, this.curRenderWidth, this.curRenderWidth};
+			int[] yPts = new int[]{0, this.curRenderHeight, this.curRenderHeight, 0};
+			
+			//Transform points
+			VecMath.transformPoints(mat, DRAWING_MATRIX, xPts, yPts, xPts, yPts);
+			
+			//Draw "shadow"
+			graphics.setColor(Color.WHITESMOKE);
+			graphics.setGlobalAlpha(60);
+			graphics.translate(4, 4); //Offset the shadow a bit
+			graphics.drawFilledPath(xPts, yPts, null, null);
+			graphics.translate(-4, -4);
+			graphics.setGlobalAlpha(255);
+			
+			//Get UV points for texture
+			int dux = Fixed32.div(Fixed32.ONE, Fixed32.div(Fixed32.toFP(this.curRenderWidth), Fixed32.toFP(extent.width)));
+			int dvy = Fixed32.div(Fixed32.ONE, Fixed32.div(Fixed32.toFP(this.curRenderHeight), Fixed32.toFP(extent.height)));
+			
+			//Draw
+			graphics.drawTexturedPath(xPts, yPts, null, null, 0, 0, dux, 0, 0, dvy, this.drawnPage);
 		}
 		else
 		{
 			//No PDF to draw
-			XYRect extent = this.getExtent();
 			String text = "No page selected";
 			
 			graphics.setColor(Color.BLACK);
 			graphics.drawText(text, 0, (extent.height >> 1) - (graphics.getFont().getHeight() >> 1), Graphics.HCENTER, extent.width);
 		}
 		
+		graphics.setGlobalAlpha(palpha);
 		graphics.setColor(pcolor);
 	}
 	
@@ -100,7 +120,10 @@ public class FullPageField extends GestureField
 	
 	protected boolean interactionScale(int x, int y)
 	{
+		needRerender = true;
 		GestureField.matrixSetScale(mat, WORKING_MATRIX, x, y);
+		sW = Fixed32.mul(sW, x);
+		sH = Fixed32.mul(sH, y);
 		return applyMatrix(false);
 	}
 	
@@ -121,8 +144,29 @@ public class FullPageField extends GestureField
 	
 	protected void interactionComplete()
 	{
-		//TODO: Stop any rendering, start again with new settings (the only time it should need to do this is with scaling; translation and rotation won't change the rendering so it can remain the same)
-		invalidate();
+		if(this.needRerender)
+		{
+			if(this.page != null)
+			{
+				if(!this.page.isFinished())
+				{
+					this.page.stop(this.curRenderWidth, this.curRenderHeight, null);
+					try
+					{
+						this.page.waitForFinish();
+					}
+					catch (InterruptedException e)
+					{
+					}
+				}
+				renderPDF();
+			}
+			this.needRerender = false;
+		}
+		else
+		{
+			invalidate();
+		}
 	}
 	
 	public void fitToField()
@@ -138,19 +182,62 @@ public class FullPageField extends GestureField
 	
 	public synchronized void showPage(PDFPage page)
 	{
-		//TODO: Start the rendering process for the page
-		String txt = page == null ? "null" : Integer.toString(page.getPageNumber());
-		System.out.println("FullPageField: " + txt);
+		if(this.page != page)
+		{
+			if(this.page != null)
+			{
+				if(!this.page.isFinished())
+				{
+					this.page.stop(this.curRenderWidth, this.curRenderHeight, null);
+					try
+					{
+						this.page.waitForFinish();
+					}
+					catch (InterruptedException e)
+					{
+					}
+				}
+			}
+			this.page = page;
+			renderPDF();
+		}
+	}
+	
+	private Bitmap setupDraw()
+	{
+		//Initialize the drawing surface
+		this.curRenderWidth = Fixed32.toInt(sW);
+		this.curRenderHeight = Fixed32.toInt(sH);
+//#ifdef BlackBerrySDK4.5.0 | BlackBerrySDK4.6.0 | BlackBerrySDK4.6.1 | BlackBerrySDK4.7.0 | BlackBerrySDK4.7.1 | BlackBerrySDK5.0.0 | BlackBerrySDK6.0.0
+		Bitmap bmp = new Bitmap(Bitmap.ROWWISE_16BIT_COLOR, this.curRenderWidth, this.curRenderHeight);
+//#else
+		Bitmap bmp = new Bitmap(Bitmap.ROWWISE_32BIT_XRGB8888, this.curRenderWidth, this.curRenderHeight);
+//#endif
+		
+		return bmp;
 	}
 	
 	private void renderPDF()
 	{
-		//TODO: Run rendering in background, when done notify rendering complete.
-	}
-	
-	private void renderingComplete()
-	{
-		//TODO: Display rendered PDF
+		this.gestureProcessing = this.page != null;
+		if(this.page != null)
+		{
+			Thread thread = new Thread(new Runnable()
+			{
+				public void run()
+				{
+					FullPageField fpf = FullPageField.this;
+					Bitmap bmp = fpf.setupDraw();
+					fpf.page.drawTo(bmp, bmp.getWidth(), bmp.getHeight(), null, true, true);
+					if(fpf.page.isFinished())
+					{
+						fpf.drawnPage = bmp;
+						fpf.invalidate();
+					}
+				}
+			});
+			thread.start();
+		}
 	}
 	
 	protected boolean interactionGesture(Gesture gesture)
